f5c26bdb73c31837777013bc4701f33fec476eba
Lotho <58112436+MLotho@users.noreply.github.com>
2022-02-22 15:16:14 +0000

    added input options to command handler

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,7 +2,11 @@ module.exports = {
   name: "ping",
   description: "Pong",
 
+  options: [
+    { name: "player", type: "USER", description: "Say hey to a user", required: true }
+  ],
+
   run: async (bot, interaction) => {
-    interaction.reply("Hello!");
+    interaction.reply(`Pong`)
   }
 }
